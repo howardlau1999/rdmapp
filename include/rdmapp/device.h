b29338699179f113aebad7d118422521cfa6af6a
Howard Lau <howardlau1999@hotmail.com>
2022-08-10 15:05:52 +0800

    device: open device by name

--- include/rdmapp/device.h
+++ include/rdmapp/device.h
@@ -1,12 +1,41 @@
 #pragma once
 
 #include <cstdint>
+#include <iterator>
+#include <string>
 
 #include <infiniband/verbs.h>
 
 #include "rdmapp/detail/noncopyable.h"
 
 namespace rdmapp {
+
+class device_list : public noncopyable {
+  struct ibv_device **devices_;
+  int32_t nr_devices_;
+
+public:
+  class iterator
+      : public std::iterator<std::forward_iterator_tag, struct ibv_device *> {
+    friend class device_list;
+    size_t i_;
+    struct ibv_device **devices_;
+    iterator(struct ibv_device **devices, size_t i);
+
+  public:
+    struct ibv_device *&operator*();
+    bool operator==(device_list::iterator const &other) const;
+    bool operator!=(device_list::iterator const &other) const;
+    device_list::iterator &operator++();
+    device_list::iterator &operator++(int);
+  };
+  device_list();
+  size_t size();
+  struct ibv_device *at(size_t i);
+  iterator begin();
+  iterator end();
+  ~device_list();
+};
 
 class device : public noncopyable {
   struct ibv_device *device_;
@@ -18,8 +47,11 @@ class device : public noncopyable {
   friend class cq;
   friend class qp;
   friend class srq;
+  void open_device(struct ibv_device *target, uint16_t port_num);
 
 public:
+  device(struct ibv_device *target, uint16_t port_num = 1);
+  device(std::string const &device_name, uint16_t port_num = 1);
   device(uint16_t device_num = 0, uint16_t port_num = 1);
   uint16_t port_num();
   uint16_t lid();
--- src/device.cc
+++ src/device.cc
@@ -1,5 +1,6 @@
 #include "rdmapp/device.h"
 
+#include <cassert>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
@@ -12,27 +13,70 @@
 
 namespace rdmapp {
 
-device::device(uint16_t device_num, uint16_t port_num) : device_(nullptr) {
-  int32_t nr_devices = 0;
-  struct ibv_device **ibv_device_list = ::ibv_get_device_list(&nr_devices);
-  if (nr_devices == 0) {
+device_list::device_list() : devices_(nullptr), nr_devices_(0) {
+  devices_ = ::ibv_get_device_list(&nr_devices_);
+  if (nr_devices_ == 0) {
     throw std::runtime_error("no Infiniband devices found");
   }
-  check_ptr(ibv_device_list, "failed to get Infiniband devices");
-  if (device_num >= nr_devices) {
-    char buffer[256] = {0};
-    ::snprintf(buffer, sizeof(buffer),
-               "requested device number %d out of range, %d devices available",
-               device_num, nr_devices);
-    throw std::invalid_argument(buffer);
+  check_ptr(devices_, "failed to get Infiniband devices");
+}
+
+device_list::~device_list() {
+  if (devices_ != nullptr) {
+    ::ibv_free_device_list(devices_);
   }
-  device_ = ibv_device_list[device_num];
+}
+
+device_list::iterator::iterator(struct ibv_device **devices, size_t i)
+    : i_(i), devices_(devices) {}
+
+struct ibv_device *&device_list::iterator::operator*() {
+  return devices_[i_];
+}
+
+bool device_list::iterator::operator==(
+    device_list::iterator const &other) const {
+  return i_ == other.i_;
+}
+
+bool device_list::iterator::operator!=(
+    device_list::iterator const &other) const {
+  return i_ != other.i_;
+}
+
+device_list::iterator &device_list::iterator::operator++() {
+  i_++;
+  return *this;
+}
+
+device_list::iterator &device_list::iterator::operator++(int) {
+  i_++;
+  return *this;
+}
+
+device_list::iterator device_list::begin() { return iterator(devices_, 0); }
+
+device_list::iterator device_list::end() {
+  return iterator(devices_, nr_devices_);
+}
+
+size_t device_list::size() { return nr_devices_; }
+
+struct ibv_device *device_list::at(size_t i) {
+  if (i >= nr_devices_) {
+    throw std::out_of_range("out of range");
+  }
+  return devices_[i];
+}
+
+
+void device::open_device(struct ibv_device *target, uint16_t port_num) {
+  device_ = target;
+  port_num_ = port_num;
   ctx_ = ::ibv_open_device(device_);
   check_ptr(ctx_, "failed to open device");
-  ::ibv_free_device_list(ibv_device_list);
-  check_rc(::ibv_query_port(ctx_, port_num, &port_attr_),
+  check_rc(::ibv_query_port(ctx_, port_num_, &port_attr_),
            "failed to query port");
-  port_num_ = port_num;
   const char *link_layer = "unspecified";
   if (port_attr_.link_layer == IBV_LINK_LAYER_ETHERNET) {
     link_layer = "ethernet";
@@ -41,6 +85,37 @@ device::device(uint16_t device_num, uint16_t port_num) : device_(nullptr) {
   }
   RDMAPP_LOG_DEBUG("opened Infiniband device lid=%d link_layer=%s",
                    port_attr_.lid, link_layer);
+}
+
+device::device(struct ibv_device *target, uint16_t port_num) {
+  assert(target != nullptr);
+  open_device(target, port_num);
+}
+
+device::device(std::string const &device_name, uint16_t port_num)
+    : device_(nullptr), port_num_(0) {
+  int32_t nr_devices = 0;
+  auto devices = device_list();
+  for (auto target : devices) {
+    if (::ibv_get_device_name(target) == device_name) {
+      open_device(target, port_num);
+      return;
+    }
+  }
+  throw_with("no device named %s found", device_name.c_str());
+}
+
+device::device(uint16_t device_num, uint16_t port_num)
+    : device_(nullptr), port_num_(0) {
+  auto devices = device_list();
+  if (device_num >= devices.size()) {
+    char buffer[256] = {0};
+    ::snprintf(buffer, sizeof(buffer),
+               "requested device number %d out of range, %lu devices available",
+               device_num, devices.size());
+    throw std::invalid_argument(buffer);
+  }
+  open_device(devices.at(device_num), port_num);
 }
 
 uint16_t device::port_num() { return port_num_; }
