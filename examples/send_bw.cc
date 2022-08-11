47f4519fa88db6afb0363b556ed2713b04ed4bec
Howard Lau <howardlau1999@hotmail.com>
2022-08-11 19:53:20 +0800

    add send_bw:

---
+++ examples/send_bw.cc
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <rdmapp/rdmapp.h>
+#include <string>
+#include <thread>
+
+using namespace std::literals::chrono_literals;
+
+rdmapp::task<void> handle_qp(std::shared_ptr<rdmapp::qp> qp) {
+
+  co_return;
+}
+
+rdmapp::task<void> server(rdmapp::acceptor &acceptor) {
+  while (true) {
+    auto qp = co_await acceptor.accept();
+    handle_qp(qp).detach();
+  }
+  co_return;
+}
+
+rdmapp::task<void> client(rdmapp::connector &connector) {
+  auto qp = co_await connector.connect();
+  co_return;
+}
+
+int main(int argc, char *argv[]) {
+  auto device = std::make_shared<rdmapp::device>(0, 1);
+  auto pd = std::make_shared<rdmapp::pd>(device);
+  auto cq = std::make_shared<rdmapp::cq>(device);
+  auto cq_poller = std::make_shared<rdmapp::cq_poller>(cq);
+  auto loop = rdmapp::socket::event_loop::new_loop();
+  auto looper = std::thread([loop]() { loop->loop(); });
+  if (argc == 2) {
+    rdmapp::acceptor acceptor(loop, std::stoi(argv[1]), pd, cq);
+    auto coro = server(acceptor);
+    coro.get_future().wait();
+    if (auto exception = coro.get_exception()) {
+      std::rethrow_exception(exception);
+    }
+  } else if (argc == 3) {
+    rdmapp::connector connector(loop, argv[1], std::stoi(argv[2]), pd, cq);
+    auto coro = client(connector);
+    coro.get_future().wait();
+    if (auto exception = coro.get_exception()) {
+      std::rethrow_exception(exception);
+    }
+  } else {
+    std::cout << "Usage: " << argv[0] << " [port] for server and " << argv[0]
+              << " [server_ip] [port] for client" << std::endl;
+  }
+  loop->close();
+  looper.join();
+  return 0;
+}
