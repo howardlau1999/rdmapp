9f4143ef5aca7c213f0bb1ecb38c40194821188c
Howard Lau <howardlau1999@hotmail.com>
2022-08-13 21:59:18 +0800

    examples: send_bw refactor

--- examples/send_bw.cc
+++ examples/send_bw.cc
@@ -17,79 +17,58 @@ constexpr size_t kWorkerCount = 4;
 constexpr size_t kSendCount = 8 * 1024;
 constexpr size_t kTotalSize = kBufferSize * kSendCount * kWorkerCount;
 
-rdmapp::task<void> server_worker(size_t id, std::shared_ptr<rdmapp::qp> qp) {
+template <bool Client = false>
+rdmapp::task<void> worker(size_t id, std::shared_ptr<rdmapp::qp> qp) {
   std::vector<uint8_t> buffer;
   buffer.resize(kBufferSize);
   auto local_mr = std::make_shared<rdmapp::local_mr>(
       std::move(qp->pd_ptr()->reg_mr(&buffer[0], buffer.size())));
   std::cout << "Worker " << id << " started" << std::endl;
   for (size_t i = 0; i < kSendCount; ++i) {
-    co_await qp->send(local_mr);
+    if (Client) {
+      co_await qp->recv(local_mr);
+    } else {
+      co_await qp->send(local_mr);
+    }
     if ((i + 1) % 1024 == 0) {
-      std::cout << "Worker " << id << " sent " << (i + 1) << " times"
-                << std::endl;
+      std::cout << "Worker " << id << (Client ? " recv " : " sent ") << (i + 1)
+                << " times" << std::endl;
     }
   }
   std::cout << "Worker " << id << " exited" << std::endl;
+  co_return;
+}
+
+template <bool Client = false>
+rdmapp::task<void> handler(std::shared_ptr<rdmapp::qp> qp) {
+  std::vector<std::shared_future<void>> futures;
+  for (size_t i = 0; i < kWorkerCount; ++i) {
+    auto task = worker<Client>(i, qp);
+    futures.emplace_back(task.get_future());
+    task.detach();
+  }
+  auto tik = std::chrono::high_resolution_clock::now();
+  for (auto &fut : futures) {
+    fut.get();
+  }
+  auto tok = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> seconds = tok - tik;
+  double gb = static_cast<double>(kTotalSize) / 1024 / 1024 / 1024;
+  double throughput = gb / seconds.count();
+  std::cout << "Total: " << gb << " GB, Elapsed: " << seconds.count()
+            << "s, Throughput: " << throughput << "GB/s" << std::endl;
   co_return;
 }
 
 rdmapp::task<void> server(rdmapp::acceptor &acceptor) {
   auto qp = co_await acceptor.accept();
-  std::vector<std::shared_future<void>> futures;
-  auto tik = std::chrono::high_resolution_clock::now();
-  for (size_t i = 0; i < kWorkerCount; ++i) {
-    auto task = server_worker(i, qp);
-    futures.emplace_back(task.get_future());
-    task.detach();
-  }
-  for (auto &fut : futures) {
-    fut.get();
-  }
-  auto tok = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> seconds = tok - tik;
-  double mb = static_cast<double>(kTotalSize) / 1024 / 1024;
-  double throughput = mb / seconds.count();
-  std::cout << "Total: " << mb << " MB, Elapsed: " << seconds.count()
-            << "s, Throughput: " << throughput << "MB/s" << std::endl;
-  co_return;
-}
-
-rdmapp::task<void> client_worker(size_t id, std::shared_ptr<rdmapp::qp> qp) {
-  std::vector<uint8_t> buffer;
-  buffer.resize(kBufferSize);
-   auto local_mr = std::make_shared<rdmapp::local_mr>(
-      std::move(qp->pd_ptr()->reg_mr(&buffer[0], buffer.size())));
-  std::cout << "Worker " << id << " started" << std::endl;
-  for (size_t i = 0; i < kSendCount; ++i) {
-    co_await qp->recv(local_mr);
-    if ((i + 1) % 1024 == 0) {
-      std::cout << "Worker " << id << " recv " << (i + 1) << " times"
-                << std::endl;
-    }
-  }
-  std::cout << "Worker " << id << " exited" << std::endl;
+  co_await handler(qp);
   co_return;
 }
 
 rdmapp::task<void> client(rdmapp::connector &connector) {
   auto qp = co_await connector.connect();
-  std::vector<std::shared_future<void>> futures;
-  for (size_t i = 0; i < kWorkerCount; ++i) {
-    auto task = client_worker(i, qp);
-    futures.emplace_back(task.get_future());
-    task.detach();
-  }
-  auto tik = std::chrono::high_resolution_clock::now();
-  for (auto &fut : futures) {
-    fut.get();
-  }
-  auto tok = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> seconds = tok - tik;
-  double mb = static_cast<double>(kTotalSize) / 1024 / 1024;
-  double throughput = mb / seconds.count();
-  std::cout << "Total: " << mb << " MB, Elapsed: " << seconds.count()
-            << "s, Throughput: " << throughput << "MB/s" << std::endl;
+  co_await handler<true>(qp);
   co_return;
 }
 
